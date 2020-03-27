a94084ac90bb2c25e931b460125bc81d9d8a1b4f
lucasdpau <lucasdpau@gmail.com>
2020-03-27 16:34:39 -0400

    add views, ejs template engine

--- server-exp.js
+++ server-exp.js
@@ -1,15 +1,24 @@
 // Load Express
 const express = require('express');
+const path = require('path');
 // Create our express app
 const app = express();
 
 // Configure the app (app.set)
+// ejs is installed by npm. it is a view templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Mount middleware (app.use)
 
 // Define routes
 app.get('/', (request, response) => {
     response.send("<h1>Hello World!</h1>");
+});
+
+// This will render the home.ejs template
+app.get('/home', (req, res) => {
+    res.render('home');
 });
 
 // Tell the app to listen on port 8080
