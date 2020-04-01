41240d27fc82e8459696bef97472f0935c51c882
Lucas <lucasdpau@gmail.com>
2020-03-31 23:16:18 -0400

    add low level node practice

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var http = require('http');  //imports the http module
 var dt = require('./exportdate');
+var url = require('url');
+var fs = require('fs');
 
 //The function passed into http.createServer will be executed when someone tries
 //to access the computer at port 8080.
 const server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+  fs.readFile('./views/index.html', function(err, data) { 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var query_url = req.url;
+    res.write(data);
+    res.end();
+  });
   });
 
 server.listen(8080, "127.0.0.1", () => {
