77d2299976519e340d03b50550f019a709b6bd02
lp <lucasdpau>
2020-04-30 14:22:10 -0400

    new entry page created.

--- models/food.js
+++ models/food.js
@@ -7,7 +7,7 @@ var FoodSchema = new Schema(
         food_name: {type: String, required: true},
         datetime_eaten: {type: Date, required: true},
         description: {type: String, default: "No description yet"},
-        tags: {type: String},
+        tags: {type: Array },
         user: {type: Schema.Types.ObjectId, ref: 'Users', required:true},
 
     }
