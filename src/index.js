6ac09d9964bc3193136a5c8dbe8a80dd176bf365
Lucas <lucasdpau@gmail.com>
2020-06-02 21:42:08 -0400

    add full calendar interactive.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ]
+    plugins: [ dayGridPlugin, interactionPlugin ],
+
+    dateClick: function(info) {
+      alert('Clicked on: ' + info.dateStr);
+    }
   });
 
   calendar.render();
