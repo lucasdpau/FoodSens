441d4990e2bef287895b31c512542aff50d099aa
Lucas <lucasdpau@gmail.com>
2020-08-27 19:26:05 -0400

    remove react crossover from home.ejs. bundle all react in homereact.js. made homereact.js use import

--- src/homereact.js
+++ src/homereact.js
@@ -1,7 +1,8 @@
-var  React = require('react');
-var  ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from "react-dom";
 
 const Ele = React.createElement;
 
 const element = Ele('div',null, 'Hello World!');
 ReactDOM.render(element, document.getElementById('test'));
+console.log("react is working!");
