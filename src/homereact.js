7519624dfd5e53d61b90ede6acbb0d5317da24cf
Lucas <lucasdpau@gmail.com>
2020-08-27 12:37:50 -0400

    add react and react dom modules. webpack config does homereact

--- src/homereact.js
+++ src/homereact.js
@@ -1,2 +1,7 @@
-const element = <h1>Hello, world</h1>;
+var  React = require('react');
+var  ReactDOM = require('react-dom');
+
+const Ele = React.createElement;
+
+const element = Ele('div',null, 'Hello World!');
 ReactDOM.render(element, document.getElementById('test'));
--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,11 @@ module.exports = [
     path: path.resolve(__dirname, 'public'),
   },
 },
+{
+  entry: './src/homereact.js',
+  output: {
+    filename: 'homereact.js', 
+    path: path.resolve(__dirname, 'public'),
+  },
+},
 ];
